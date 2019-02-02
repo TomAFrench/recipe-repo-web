95e6ed60d3a969e2029f382c1a00f6a586c39482
Tom French <tom@tomfren.ch>
2019-02-02 00:23:10 +0000

    Remove unused style

--- src/Recipe/RecipeViewer.js
+++ src/Recipe/RecipeViewer.js
@@ -25,16 +25,6 @@ const styles = theme => ({
     width: '100px',
     margin: 'auto'
   },
-  paper: {
-    marginTop: theme.spacing.unit * 3,
-    marginBottom: theme.spacing.unit * 3,
-    padding: theme.spacing.unit * 2,
-    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
-      marginTop: theme.spacing.unit * 6,
-      marginBottom: theme.spacing.unit * 6,
-      padding: theme.spacing.unit * 3,
-    },
-  },
   cardGrid: {
     padding: `${theme.spacing.unit * 8}px 0`,
   },
