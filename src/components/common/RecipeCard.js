b10b10f9773d3c1051100bb54e84230ce542e4f9
Tom French <tom@tomfren.ch>
2019-03-31 22:20:49 +0100

    standard formatting

--- src/components/common/RecipeCard.js
+++ src/components/common/RecipeCard.js
@@ -1,81 +1,80 @@
-import React from 'react';
+import React from 'react'
 import { NavLink } from 'react-router-dom'
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
 
 const styles = () => ({
   card: {
     height: '100%',
     display: 'flex',
-    flexDirection: 'column',
+    flexDirection: 'column'
   },
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
+    paddingTop: '56.25%' // 16:9
   },
   cardContent: {
-    flexGrow: 1,
-  },
-});
+    flexGrow: 1
+  }
+})
 
 class RecipeCard extends React.Component {
-
-  constructor(props){
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      image: '',
+      image: ''
     }
   }
 
-  componentDidMount(){
-    this.decodeImage();
+  componentDidMount () {
+    this.decodeImage()
   }
 
-  async decodeImage() {
-    if ("image" in this.props.recipe) {
-      var binary = '';
-      var bytes = [].slice.call(new Uint8Array(this.props.recipe.image.data.data));
+  async decodeImage () {
+    if ('image' in this.props.recipe) {
+      var binary = ''
+      var bytes = [].slice.call(new Uint8Array(this.props.recipe.image.data.data))
 
-      bytes.forEach((b) => binary += String.fromCharCode(b));
+      bytes.forEach((b) => (binary += String.fromCharCode(b)))
 
-      var recipeImage = "data:"+ this.props.recipe.image.contentType + ";base64," + window.btoa(binary);
-      this.setState({image: recipeImage})
+      var recipeImage = 'data:' + this.props.recipe.image.contentType + ';base64,' + window.btoa(binary)
+      this.setState({ image: recipeImage })
     }
   };
 
-  render() {
+  render () {
     return (
-    <Card className={this.props.classes.card}>
-      <CardMedia
-        className={this.props.classes.cardMedia}
-        image={this.state.image}
-        title={this.props.recipe.name}
+      <Card className={this.props.classes.card}>
+        <CardMedia
+          className={this.props.classes.cardMedia}
+          image={this.state.image}
+          title={this.props.recipe.name}
         />
-      <CardContent className={this.props.classes.cardContent}>
-        <Typography gutterBottom variant="h5" component="h2">
-          {this.props.recipe.name}
-        </Typography>
-        <Typography>
-          {this.props.recipe.sourceUrl}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" color="primary" component={NavLink} to={`/recipe/${this.props.recipe._id}`}>
-          View
-        </Button>
-      </CardActions>
-    </Card>
+        <CardContent className={this.props.classes.cardContent}>
+          <Typography gutterBottom variant='h5' component='h2'>
+            {this.props.recipe.name}
+          </Typography>
+          <Typography>
+            {this.props.recipe.sourceUrl}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size='small' color='primary' component={NavLink} to={`/recipe/${this.props.recipe._id}`}>
+            View
+          </Button>
+        </CardActions>
+      </Card>
     )
   }
 }
 
 RecipeCard.propTypes = {
-  recipe: PropTypes.object.isRequired,
-};
+  recipe: PropTypes.object.isRequired
+}
 
-export default withStyles(styles)(RecipeCard);
+export default withStyles(styles)(RecipeCard)
