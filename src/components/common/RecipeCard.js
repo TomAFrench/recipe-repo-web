5871e2c7b2b0c085666bfa7846c1305f3b31349d
Tom French <tom@tomfren.ch>
2019-03-31 22:22:42 +0100

    improved imports in RecipeCard

--- src/components/common/RecipeCard.js
+++ src/components/common/RecipeCard.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import Button from '@material-ui/core/Button'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Typography from '@material-ui/core/Typography'
+import { Button, Typography, Card, CardActions, CardContent, CardMedia } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 
 const styles = () => ({
