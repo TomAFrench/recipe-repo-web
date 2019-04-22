f536b6f91cfd8b73019feb99db4d206cdb106b53
Tom French <tom@tomfren.ch>
2019-04-22 16:06:11 +0100

    Add: RecipeDisplay now has a min size on large screens

--- src/components/recipe/view/RecipeDisplay.js
+++ src/components/recipe/view/RecipeDisplay.js
@@ -9,7 +9,8 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 3,
     marginBottom: theme.spacing.unit * 3,
     padding: theme.spacing.unit * 2,
-    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+    [theme.breakpoints.up(1000 + theme.spacing.unit * 3 * 2)]: {
+      minWidth: 1000,
       marginTop: theme.spacing.unit * 6,
       marginBottom: theme.spacing.unit * 6,
       padding: theme.spacing.unit * 3
