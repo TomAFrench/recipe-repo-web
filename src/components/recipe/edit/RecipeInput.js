b4ffe99ce70434ab25140ab241c08682f643430b
Tom French <tom@tomfren.ch>
2019-05-12 20:57:07 +0100

    Refactor: remove unneeded import

--- src/components/recipe/edit/RecipeInput.js
+++ src/components/recipe/edit/RecipeInput.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import base64Img from 'base64-img'
 
 import { Paper, Button, Grid, Typography, TextField } from '@material-ui/core'
 
